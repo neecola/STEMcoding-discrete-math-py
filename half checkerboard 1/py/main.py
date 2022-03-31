5b2c98e2e6555d32979b1bdf111cbd45c506a134
Nicola <nneecola@gmail.com>
2022-03-31 12:04:18 -0400

    Added example project working on p5js.org

--- half checkerboard 1/py/main.py
+++ half checkerboard 1/py/main.py
@@ -1,24 +1,25 @@
-from processing import *
-from functions import *
+#from pyp5js import *
+#from functions import *
 
-xchecker1 = 50
-ychecker1 = 50
-
-showaxes = False
-
-
-def draw():
-    global xchecker1,ychecker1
-    clearscreen(255, 255, 255)
-    #display()
-    
-    if keyPressed is True and key == 1:
-      ychecker1 += 100
-    
-    
-    fill(0);
-    ellipse(xchecker1,ychecker1,50,50)
-#    display(x,y,vx,vy,deltaVx,deltaVy,theta,mass)
-
-run()
-
+#xchecker1 = 50
+#ychecker1 = 50
+#
+#showaxes = False
+#
+#
+#def draw():
+#    global xchecker1,ychecker1
+#    clearscreen(255, 255, 255)
+#    #display()
+#    
+#    if keyPressed is True and key == 1:
+#      ychecker1 += 100
+#    
+#    
+#    fill(0);
+#    ellipse(xchecker1,ychecker1,50,50)
+##    display(x,y,vx,vy,deltaVx,deltaVy,theta,mass)
+#
+#run()
+#
+#
---
+++ working pyp5js page/functions.js
@@ -0,0 +1,13 @@
+let functionsCode = `
+
+#def testfcn():
+#    createP(3.14);
+
+
+
+
+
+`;
+
+let userCode = sketchCode.concat(functionsCode);
+
